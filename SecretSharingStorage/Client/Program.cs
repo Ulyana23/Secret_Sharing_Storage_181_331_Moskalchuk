bec26fb245f70d2acf291503611e6daab512763a
Ulyana23 <43664758+Ulyana23@users.noreply.github.com>
2023-05-04 17:56:16 +0300

    add Client and Server2

---
+++ SecretSharingStorage/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect("127.0.0.1", 8001);
+                Console.WriteLine("Client connected.");
+
+                Console.Write("Введите сообщение: ");
+                string message = Console.ReadLine();
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                clientSocket.Send(data);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+                Console.WriteLine("Client closed.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SecretSharingStorage/Server2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Data.SQLite;
+
+namespace Server2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress destinationIpAddress = IPAddress.Parse("127.0.0.1");
+            int destinationPort = 8002;
+
+            Socket destinationSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            destinationSocket.Bind(new IPEndPoint(destinationIpAddress, destinationPort));
+            destinationSocket.Listen(1);
+            Console.WriteLine("Сервер 2 запущен. Ожидание подключений...");
+
+            Socket sourceSocket = destinationSocket.Accept();
+            Console.WriteLine("Сервер 2 подключен к серверу 1.");
+
+            byte[] buffer = new byte[1024];
+            int length = sourceSocket.Receive(buffer);
+            string message = Encoding.UTF8.GetString(buffer, 0, length);
+            Console.WriteLine("Получено сообщение: " + message);
+
+            // Закрываем соединения
+            //destinationSocket.Shutdown(SocketShutdown.Both);
+            destinationSocket.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
