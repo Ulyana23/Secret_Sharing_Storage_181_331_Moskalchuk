a44ac2de7c985648c8be580a1aa08e6c24e60f16
Ulyana23 <43664758+Ulyana23@users.noreply.github.com>
2023-05-09 13:59:49 +0300

    implemented 5 servers and writing to the database

--- SecretSharingStorage/Client/Program.cs
+++ SecretSharingStorage/Client/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace Client
 {
@@ -9,24 +10,95 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            int clientId = 1;
             try
             {
                 Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 clientSocket.Connect("127.0.0.1", 8001);
-                Console.WriteLine("Client connected.");
+                //Console.WriteLine("Client connected.");
 
-                Console.Write("Введите сообщение: ");
-                string message = Console.ReadLine();
+                JObject json = new JObject();
+                string message = "";
+                string jsonString = "";
+                string choice = "";
 
-                byte[] data = Encoding.UTF8.GetBytes(message);
+                while (true)
+                {
+                    Console.WriteLine("Выберите один из трёх вариантов:");
+                    Console.WriteLine("1. Я хочу добавить новый секрет в хранилище");
+                    Console.WriteLine("2. Я хочу получить секрет из хранилища");
+                    Console.WriteLine("3. Выход");
+                    choice = Console.ReadLine();
 
-                clientSocket.Send(data, 0, data.Length, SocketFlags.None);
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.WriteLine("Вы выбрали добавить новый секрет в хранилище");
+                            Console.Write("Введите сообщение: ");
+                            message = Console.ReadLine();
 
-                // Получаем буфер для чтения ответа от сервера
-                data = new byte[1024];
-                int bytesRead = clientSocket.Receive(data);
-                string rMessage = Encoding.ASCII.GetString(data, 0, bytesRead);
-                Console.WriteLine("Получен ответ от сервера: " + rMessage);
+                            // Добавляем параметры json
+                            json.Add("clientId", clientId);
+                            json.Add("message", message);
+                            json.Add("type", "set");
+
+                            // Преобразуем объект JObject в строку json
+                            jsonString = json.ToString();
+
+                            byte[] dataSet = Encoding.UTF8.GetBytes(jsonString);
+
+                            clientSocket.Send(dataSet, 0, dataSet.Length, SocketFlags.None);
+
+                            // Получаем буфер для чтения ответа от сервера
+                            dataSet = new byte[1024];
+                            int bytesRead = clientSocket.Receive(dataSet);
+                            string secretId = Encoding.ASCII.GetString(dataSet, 0, bytesRead);
+                            if (secretId == "false")
+                            {
+                                Console.WriteLine("К сожалению, сервера сейчас недоступны, попробуйте позже.\r\n");
+                                return;
+                            }
+                            else Console.WriteLine("ID вашего секрета: " + secretId + "\r\n");
+                            break;
+
+                        case "2":
+                            Console.WriteLine("Вы выбрали получить секрет из хранилища");
+                            Console.Write("Введите ID вашего секрета: ");
+                            message = Console.ReadLine();
+
+                            // Добавляем параметры json
+                            json.Add("clientId", clientId);
+                            json.Add("message", message);
+                            json.Add("type", "get");
+
+                            // Преобразуем объект JObject в строку json
+                            jsonString = json.ToString();
+
+                            byte[] dataGet = Encoding.UTF8.GetBytes(jsonString);
+
+                            clientSocket.Send(dataGet, 0, dataGet.Length, SocketFlags.None);
+
+                            // Получаем буфер для чтения ответа от сервера
+                            dataSet = new byte[1024];
+                            int bytesRead2 = clientSocket.Receive(dataSet);
+                            string secret = Encoding.ASCII.GetString(dataSet, 0, bytesRead2);
+                            if (secret == "false")
+                            {
+                                Console.WriteLine("К сожалению, сервера сейчас недоступны, попробуйте позже.\r\n");
+                                return;
+                            }
+                            else Console.WriteLine("Ваш секрет: " + secret + "\r\n");
+
+                            break;
+                        case "3":
+                            Console.WriteLine("Выход...");
+                            return;
+                        default:
+                            Console.WriteLine("Некорректный выбор");
+                            break;
+                    }
+                    json.RemoveAll();
+                }
 
                 clientSocket.Shutdown(SocketShutdown.Both);
                 clientSocket.Close();
@@ -38,7 +110,6 @@ namespace Client
             }
 
             Console.ReadLine();
-
         }
     }
 }
